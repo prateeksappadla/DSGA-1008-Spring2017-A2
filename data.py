b974eb5c25138b8f05d6e6bff24494d5a7128209
Prateek Sappadla <pvs266@nyu.edu>
2017-04-03 16:56:47 -0400

    Added code to modify vocab size

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import collections
 
 class Dictionary(object):
     def __init__(self):
@@ -17,11 +18,35 @@ class Dictionary(object):
 
 
 class Corpus(object):
-    def __init__(self, path):
+    def __init__(self, path, vocab_size=10000):
+        self.vocab_size = vocab_size
         self.dictionary = Dictionary()
+        self.build_vocab(path,vocab_size)
+
         self.train = self.tokenize(os.path.join(path, 'train.txt'))
         self.valid = self.tokenize(os.path.join(path, 'valid.txt'))
         self.test = self.tokenize(os.path.join(path, 'test.txt'))
+
+    def build_vocab(self, path, vocab_size):
+        words = []
+        with open(os.path.join(path, 'train.txt'), 'r', errors='ignore') as f:
+            for line in f:
+                words.extend(line.split())
+
+        with open(os.path.join(path, 'valid.txt'), 'r', errors='ignore') as f:
+            for line in f:
+                words.extend(line.split())
+        
+        with open(os.path.join(path, 'test.txt'), 'r', errors='ignore') as f:
+            for line in f:
+                words.extend(line.split())
+        
+        counter = collections.Counter(words).most_common(vocab_size - 2)
+        self.dictionary.add_word('<unk>')
+        self.dictionary.add_word('<eos>')
+        for word,_ in counter:
+            self.dictionary.add_word(word)    
+
 
     def tokenize(self, path):
         """Tokenizes a text file."""
@@ -32,9 +57,7 @@ class Corpus(object):
             for line in f:
                 words = line.split() + ['<eos>']
                 tokens += len(words)
-                for word in words:
-                    self.dictionary.add_word(word)
-
+                
         # Tokenize file content
         with open(path, 'r', errors='ignore') as f:
             ids = torch.LongTensor(tokens)
@@ -42,7 +65,10 @@ class Corpus(object):
             for line in f:
                 words = line.split() + ['<eos>']
                 for word in words:
-                    ids[token] = self.dictionary.word2idx[word]
+                    if word in self.dictionary.word2idx:
+                        ids[token] = self.dictionary.word2idx[word]
+                    else:
+                        ids[token] = self.dictionary.word2idx['<unk>']    
                     token += 1
 
         return ids
